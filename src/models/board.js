920ccfb30e50e7dc50e9bc0bdeabeb40de63a29e
leesh9098 <77374838+leesh9098@users.noreply.github.com>
2021-10-11 14:28:01 +0800

    express-2021

---
+++ src/models/board.js
@@ -0,0 +1,18 @@
+export default (sequelize, DataTypes) => {
+    const Board = sequelize.define("board", {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        }
+    });
+
+    Board.associate = function (models) {
+        models.Board.belongsTo(models.User)
+    };
+
+    return Board;
+};
