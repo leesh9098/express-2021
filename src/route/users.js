4d404f827df2b72463663bdae0402f8b32bf82ec
leesh9098 <77374838+leesh9098@users.noreply.github.com>
2021-10-11 14:35:24 +0800

    express-2021

--- src/route/users.js
+++ src/route/users.js
@@ -66,22 +66,6 @@ const initDB = async() => {
     await Board.sync();
 }
 
-// initDB();
-
-// /* User 데이터 생성 */
-// const user_sync = async () => {
-//     await User.sync({ force: true }); // { force: true }: User 초기화
-//     for (let i=0; i<1000; i++) {
-//         const hashPwd = await bcrypt.hash("test1234", 10);
-//         await User.create({
-//             name: faker.name.lastName()+faker.name.firstName(),
-//             age: getRandomInt(15,50),
-//             password: hashPwd
-//         });
-//     }
-// }
-// user_sync();
-
 // 회원 가져오기
 userRouter.get("/", async (req, res) => {
     try {
